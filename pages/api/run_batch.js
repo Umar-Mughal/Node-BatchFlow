e5f619acf98517236f54c4b8e45731b2d5c4aa0f
Umar Farooq <umar.wzbd@gmail.com>
2022-12-07 02:11:44 +0500

    deleted in apis

--- pages/api/run_batch.js
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/apçççi-routes/introduction
-
-import runBatchProcess from "./batch/executeBatch";
-
-export default async function handler(req, res) {
-  const batchOutputBuffer = await runBatchProcess()
-  const batchOutput = Buffer.from(batchOutputBuffer).toString()
-  res.status(200).json({ name: 'Umar', hello: 'hello', batchOutput })
-}
