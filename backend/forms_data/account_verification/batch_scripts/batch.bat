a41b2b7c018923cf7d6e7cdf1da08b821684459b
Umar Farooq <umar.wzbd@gmail.com>
2022-12-13 05:37:35 +0500

    batch file sleep

--- backend/forms_data/account_verification/batch_scripts/batch.bat
+++ backend/forms_data/account_verification/batch_scripts/batch.bat
@@ -1,5 +1,5 @@
 echo 0 > backend/forms_data/account_verification/status.txt
-SLEEP 10
+SLEEP 5
 echo 1 > backend/forms_data/account_verification/status.txt
-SLEEP 10
+SLEEP 5
 echo 2 > backend/forms_data/account_verification/status.txt
